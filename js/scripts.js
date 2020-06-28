b49b0d79dbb228ad08d303d6690f3cfdb9065c67
jd <duverseaujohnny21@gmail.com>
2020-06-28 13:50:13 -0400

    add branching  to the pizzaCost funtion

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,16 @@ function Pizza(topping, size) {
   this.cost = 15; 
 } 
 Pizza.prototype.pizzaCost = function() {
-
+  if((this.topping + this.size) == 2) {
+    this.cost = 10;
+  } else if ((this.topping + this.size) == 3) {
+    this.cost = 12;
+  } else if ((this.topping + this.size) == 5 ) {
+    this.cost = 17;
+  } else { 
+    this.cost = 25;
+  }
+    return this.cost;
   }
 // User Interface Logic
 $(document).ready(function() {
